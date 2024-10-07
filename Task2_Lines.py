a9c26522ccb500c48c231dcaaaa433ba5817701c
IliasFransen <127627710+IliasFransen@users.noreply.github.com>
2024-10-07 09:45:01 +0200

    fix

--- Task2_Lines.py
+++ Task2_Lines.py
@@ -2,11 +2,103 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-#set up lines by angle from point A
-#n is total line, including edges
-def Line_init (Gamma_min_angle_0, Gamma_min_angle_1, n):
-    #make an array of m angles between G-0 and G-1
-    angles = np.linspace(Gamma_min_angle_0, Gamma_min_angle_1, n)
-    return angles
 
-#print(Line_init(0.5,0.3,5))    #CHECK
+def Lines (Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9, n, x_A, y_A):
+
+
+
+    #plot characteristics
+    for i in range(n):
+
+        x = np.array([])
+        y = np.array([])
+
+        #do region 4
+
+        #figure out x-range, take 5 points between a and x_bc
+
+        x_4 = np.linspace(x_A, val_5[0][i][4], 5, endpoint = False)
+
+        #get corresponding y-values
+        gamma_min_slope_4 = math.tan(val_4[i][2]-val_4[i][3])
+
+        y_4 = gamma_min_slope_4*(x_4-x_A)+y_A
+
+        #isolate x and y out of 5 array
+
+        x_5 = np.array([])
+        y_5 = np.array([])
+
+        for j in range(i):
+            x_5 = np.append(x_5, val_5[j][i][4])
+            y_5 = np.append(y_5, val_5[j][i][5])
+
+
+        for j in range(i,n):
+            x_5 = np.append(x_5, val_5[i][j][4])
+            y_5 = np.append(y_5, val_5[i][j][5])
+                
+        #region 6
+
+        #get 5 x-points again
+        x_6 = np.linspace(val_5[i][-1][4], val_7[0][i][4], 5+1, endpoint = False)[1:]
+
+        #get slope of 6
+
+        gamma_min_slope_6 = math.tan(val_5[i][-1][2]+val_5[i][-1][3])
+
+        y_6 = gamma_min_slope_6*(x_6-val_5[i][-1][4])+val_5[i][-1][5]
+
+        #region 7
+        #isolate x and y out of 7 array
+
+        x_7 = np.array([])
+        y_7 = np.array([])
+
+        for j in range(i):
+            x_7 = np.append(x_7, val_7[j][i][4])
+            y_7 = np.append(y_7, val_7[j][i][5])
+
+
+        for j in range(i,n):
+            x_7 = np.append(x_7, val_7[i][j][4])
+            y_7 = np.append(y_7, val_7[i][j][5])
+
+        #region 8
+
+        #get 5 x-points again
+
+        x_8 = np.linspace(val_7[i][-1][4], val_9[0][i][4], 5+1, endpoint = False)[1:]
+
+        #get slope of 8
+
+        gamma_min_slope_8 = math.tan(val_7[i][-1][2]-val_7[i][-1][3])
+        
+
+        y_8 = gamma_min_slope_8*(x_8-val_7[i][-1][4])+val_7[i][-1][5]
+
+        #region 9
+
+        #isolate x and y out of 9 array
+
+        x_9 = np.array([])
+        y_9 = np.array([])
+
+        x_9 = np.array([])
+        y_9 = np.array([])
+
+        for j in range(i):
+            x_9 = np.append(x_9, val_9[j][i][4])
+            y_9 = np.append(y_9, val_9[j][i][5])
+
+
+        for j in range(i,n):
+            x_9 = np.append(x_9, val_9[i][j][4])
+            y_9 = np.append(y_9, val_9[i][j][5])
+        
+        #create one long array for x and y
+
+        x = np.concatenate((x_4, x_5, x_6, x_7, x_8, x_9))
+        y = np.concatenate((y_4, y_5, y_6, y_7, y_8, y_9))
+
+        plt.plot(x, y, color = 'black')
--- Task2_Main.py
+++ Task2_Main.py
@@ -8,7 +8,7 @@ from Task2_Regions import region_0, region_1, region_2, region_3
 from Task2_Fans4_5 import coord_B, region4, point_BC, region5_sym, region5_gen
 from Task2_Fans6_7 import coord_D, point_DF, region7_sym, region7_gen
 from Task2_Fans8_9 import region9_gen, region9_sym, coord_H, pointHK
-from Task2_Lines import Line_init
+from Task2_Lines import Lines
 from Calculator import Calculator
 
 #define variables
@@ -22,107 +22,10 @@ y_A = 1      #nozzle height (m)
 x_A = 0      #nozzle end (m)
 
 #number of lines in fans (including edges)
+lines = True
 n=6
 
-def Lines (Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9, n, x_A, y_A):
 
-
-
-    #plot characteristics
-    for i in range(n):
-
-        x = np.array([])
-        y = np.array([])
-
-        #do region 4
-
-        #figure out x-range, take 5 points between a and x_bc
-
-        x_4 = np.linspace(x_A, val_5[0][i][4], 5, endpoint = False)
-
-        #get corresponding y-values
-        gamma_min_slope_4 = math.tan(val_4[i][2]-val_4[i][3])
-
-        y_4 = gamma_min_slope_4*(x_4-x_A)+y_A
-
-        #isolate x and y out of 5 array
-
-        x_5 = np.array([])
-        y_5 = np.array([])
-
-        for j in range(i):
-            x_5 = np.append(x_5, val_5[j][i][4])
-            y_5 = np.append(y_5, val_5[j][i][5])
-
-
-        for j in range(i,n):
-            x_5 = np.append(x_5, val_5[i][j][4])
-            y_5 = np.append(y_5, val_5[i][j][5])
-                
-        #region 6
-
-        #get 5 x-points again
-        x_6 = np.linspace(val_5[i][-1][4], val_7[0][i][4], 5+1, endpoint = False)[1:]
-
-        #get slope of 6
-
-        gamma_min_slope_6 = math.tan(val_5[i][-1][2]+val_5[i][-1][3])
-
-        y_6 = gamma_min_slope_6*(x_6-val_5[i][-1][4])+val_5[i][-1][5]
-
-        #region 7
-        #isolate x and y out of 7 array
-
-        x_7 = np.array([])
-        y_7 = np.array([])
-
-        for j in range(i):
-            x_7 = np.append(x_7, val_7[j][i][4])
-            y_7 = np.append(y_7, val_7[j][i][5])
-
-
-        for j in range(i,n):
-            x_7 = np.append(x_7, val_7[i][j][4])
-            y_7 = np.append(y_7, val_7[i][j][5])
-
-        #region 8
-
-        #get 5 x-points again
-
-        x_8 = np.linspace(val_7[i][-1][4], val_9[0][i][4], 5+1, endpoint = False)[1:]
-
-        #get slope of 8
-
-        gamma_min_slope_8 = math.tan(val_7[i][-1][2]-val_7[i][-1][3])
-        
-
-        y_8 = gamma_min_slope_8*(x_8-val_7[i][-1][4])+val_7[i][-1][5]
-
-        #region 9
-
-        #isolate x and y out of 9 array
-
-        x_9 = np.array([])
-        y_9 = np.array([])
-
-        x_9 = np.array([])
-        y_9 = np.array([])
-
-        for j in range(i):
-            x_9 = np.append(x_9, val_9[j][i][4])
-            y_9 = np.append(y_9, val_9[j][i][5])
-
-
-        for j in range(i,n):
-            x_9 = np.append(x_9, val_9[i][j][4])
-            y_9 = np.append(y_9, val_9[i][j][5])
-        
-        #create one long array for x and y
-
-        x = np.concatenate((x_4, x_5, x_6, x_7, x_8, x_9))
-        y = np.concatenate((y_4, y_5, y_6, y_7, y_8, y_9))
-
-        plt.plot(x, y, color = 'black')
 
         
 
@@ -133,8 +36,9 @@ def Lines (Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9, n, x_A, y_A):
 
 def Main():
     Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9 = Calculator(M_0, phi_0, g, P_a, n, x_A, y_A)
-    Lines(Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9, n, x_A, y_A)
-    
+    if lines:
+        Lines(Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9, n, x_A, y_A)
+
     ax = plt.gca()
     ax.set_aspect('equal', adjustable='box')
     plt.show()

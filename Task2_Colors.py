87f7e1964d4640a00d5cf6132c31450a76d30e07
IliasFransen <127627710+IliasFransen@users.noreply.github.com>
2024-10-09 18:29:31 +0200

    works

--- Task2_Colors.py
+++ Task2_Colors.py
@@ -10,7 +10,13 @@ def Reg_0_color(m, Val_0, val_5, x_A, y_A):
     yi = np.linspace(0, y_A, m)
     zi = griddata(([x_A,0,val_5[0][0][4]], [y_A,0,val_5[0][0][5]]), [Val_0[1], Val_0[1], Val_0[1]], (xi[None, :], yi[:, None]), method='cubic')
 
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    x_0 = [x_A,0,val_5[0][0][4]]
+    y_0 = [y_A,0,val_5[0][0][5]]
+    M_0 = [Val_0[1], Val_0[1], Val_0[1]]
+
+    return x_0, y_0, M_0
     
 
 def Reg_1_color(m, Val_1, val_5, val_7, x_A, y_A):
@@ -18,14 +24,27 @@ def Reg_1_color(m, Val_1, val_5, val_7, x_A, y_A):
     yi = np.linspace(val_5[0][-1][5], val_7[0][0][5], m)
     zi = griddata(([x_A,val_5[0][-1][4], val_7[0][0][4]], [y_A,val_5[0][-1][5], val_7[0][0][5]]), [Val_1[1], Val_1[1], Val_1[1]], (xi[None, :], yi[:, None]), method='cubic')
 
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    x_1 = [x_A,val_5[0][-1][4], val_7[0][0][4]]
+    y_1 = [y_A,val_5[0][-1][5], val_7[0][0][5]]
+    M_1 = [Val_1[1], Val_1[1], Val_1[1]]
+
+    return x_1, y_1, M_1
 
 def Reg_2_color(m, Val_2, val_5, val_7, val_9, x_A, y_A):
     xi = np.linspace(val_5[-1][-1][4], val_9[0][0][4], m)
     yi = np.linspace(0, val_7[0][-1][5], m)
     zi = griddata(([val_5[-1][-1][4], val_7[0][-1][4], val_9[0][0][4]], [val_5[-1][-1][5], val_7[0][-1][5],val_9[0][0][5]]), [Val_2[1], Val_2[1], Val_2[1]], (xi[None, :], yi[:, None]), method='cubic')
 
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    x_2 = [val_5[-1][-1][4], val_7[0][-1][4], val_9[0][0][4]]
+    y_2 = [val_5[-1][-1][5], val_7[0][-1][5], val_9[0][0][5]]
+    M_2 = [Val_2[1], Val_2[1], Val_2[1]]
+
+    return x_2, y_2, M_2
+        
 
 def Reg_3_color(m, Val_3, val_7, val_9):
     x_7 = val_7[-1][-1][4]
@@ -43,13 +62,18 @@ def Reg_3_color(m, Val_3, val_7, val_9):
     yi = np.linspace(val_9[0][-1][5], val_7[-1][-1][5], m)
     zi = griddata(([val_7[-1][-1][4], val_9[0][-1][4], x_max], [val_7[-1][-1][5], val_9[0][-1][5], y_max]), [Val_3[1], Val_3[1], Val_3[1]], (xi[None, :], yi[:, None]), method='cubic')
 
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    x_3 = [val_7[-1][-1][4], val_9[0][-1][4], x_max]
+    y_3 = [val_7[-1][-1][5], val_9[0][-1][5], y_max]
+    M_3 = [Val_3[1], Val_3[1], Val_3[1]]
+
+    return x_3, y_3, M_3
 
 def Reg_4_colors (x_A, y_A, val_4, val_5, n):
     
     #take 100 points along each line
     m = 100
-
 
     Val_4 = np.zeros((m*n,6))
 
@@ -65,7 +89,13 @@ def Reg_4_colors (x_A, y_A, val_4, val_5, n):
     yi = np.linspace(min(Val_4[:,5]), max(Val_4[:,5]), 100)
     zi = griddata((Val_4[:,4], Val_4[:,5]), Val_4[:,1], (xi[None, :], yi[:, None]), method='cubic')
     
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    x_4 = Val_4[:,4]
+    y_4 = Val_4[:,5]
+    M_4 = Val_4[:,1]
+
+    return x_4, y_4, M_4
 
 
 def Reg_5_colors (val_5,n):
@@ -85,7 +115,9 @@ def Reg_5_colors (val_5,n):
     yi = np.linspace(min(y_5), max(y_5), 100)
     zi = griddata((x_5, y_5), M_5, (xi[None, :], yi[:, None]), method='cubic')
 
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    return x_5, y_5, M_5
 
 def Reg_6_colors (val_5, val_7, n):
     
@@ -106,7 +138,13 @@ def Reg_6_colors (val_5, val_7, n):
     yi = np.linspace(min(Val_6[:,5]), max(Val_6[:,5]), 100)
     zi = griddata((Val_6[:,4], Val_6[:,5]), Val_6[:,1], (xi[None, :], yi[:, None]), method='cubic')
     
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    x_6 = Val_6[:,4]
+    y_6 = Val_6[:,5]
+    M_6 = Val_6[:,1]
+
+    return x_6, y_6, M_6
 
 def Reg_7_colors (val_7,n):
 
@@ -125,7 +163,10 @@ def Reg_7_colors (val_7,n):
     yi = np.linspace(min(y_7), max(y_7), 100)
     zi = griddata((x_7, y_7), M_7, (xi[None, :], yi[:, None]), method='cubic')
 
-    plt.contourf(xi, yi, zi)
+    #plt.contourf(xi, yi, zi)
+
+    return x_7, y_7, M_7
+
 
 def Reg_8_colors (val_7, val_9, n):
 
@@ -144,7 +185,13 @@ def Reg_8_colors (val_7, val_9, n):
     yi = np.linspace(min(Val_8[:,5]), max(Val_8[:,5]), 100)
     zi = griddata((Val_8[:,4], Val_8[:,5]), Val_8[:,1], (xi[None, :], yi[:, None]), method='cubic')
 
-    plt.contourf(xi, yi, zi)    
+    #plt.contourf(xi, yi, zi)    
+
+    x_8 = Val_8[:,4]
+    y_8 = Val_8[:,5]
+    M_8 = Val_8[:,1]
+
+    return x_8, y_8, M_8
 
 def Reg_9_colors (val_9,n):
     
@@ -163,7 +210,7 @@ def Reg_9_colors (val_9,n):
         yi = np.linspace(min(y_9), max(y_9), 100)
         zi = griddata((x_9, y_9), M_9, (xi[None, :], yi[:, None]), method='cubic')
     
-        plt.contourf(xi, yi, zi)
+        #plt.contourf(xi, yi, zi)
 
-
+        return x_9, y_9, M_9
 
--- Task2_Main.py
+++ Task2_Main.py
@@ -4,6 +4,7 @@ import math
 from scipy.optimize import fsolve
 from scipy.interpolate import griddata
 import seaborn as sns
+import matplotlib.tri as tri
 
 from Task2_Functions import total_pressure, mach_number_pres, mach_angle, prandtl_meyer_angle, func, mach_number_nu, V_plus, V_min, Gamma_plus_angle, Gamma_min_angle
 from Task2_Regions import region_0, region_1, region_2, region_3
@@ -28,8 +29,7 @@ x_A = 0      #nozzle end (m)
 
 plot = True
 lines = True
-colors = False
-n=50
+n=7
 
 
 
@@ -38,24 +38,36 @@ n=50
 def colors (Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9, x_A, y_A, n):
 
     m = 100
-    
-    Reg_0_color(m, Val_0, val_5, x_A, y_A)
-    Reg_1_color(m, Val_1, val_5, val_7, x_A, y_A)
-    Reg_2_color(m, Val_2, val_5, val_7, val_9, x_A, y_A)
-    Reg_4_colors(x_A, y_A, val_4, val_5, n)
-    Reg_5_colors(val_5,n)
-    Reg_6_colors(val_5, val_7, n)
-    Reg_7_colors(val_7,n)
-    Reg_8_colors(val_7, val_9, n)
-    Reg_9_colors(val_9,n)
-    Reg_3_color(m, Val_3, val_7, val_9)
+    x = np.array([])
+    y = np.array([])
+    M = np.array([])
+    x_0, y_0, M_0 = Reg_0_color(m, Val_0, val_5, x_A, y_A)
+    x_1, y_1, M_1 = Reg_1_color(m, Val_1, val_5, val_7, x_A, y_A)
+    x_2, y_2, M_2 = Reg_2_color(m, Val_2, val_5, val_7, val_9, x_A, y_A)
+    x_3, y_3, M_3 = Reg_3_color(m, Val_3, val_7, val_9)
+    x_4, y_4, M_4 = Reg_4_colors(x_A, y_A, val_4, val_5, n)
+    x_5, y_5, M_5 = Reg_5_colors(val_5,n)
+    x_6, y_6, M_6 = Reg_6_colors(val_5, val_7, n)
+    x_7, y_7, M_7 = Reg_7_colors(val_7,n)
+    x_8, y_8, M_8 = Reg_8_colors(val_7, val_9, n)
+    x_9, y_9, M_9 = Reg_9_colors(val_9,n)
+
+
+    x = np.concatenate((x_0, x_1, x_2, x_3, x_4, x_5, x_6, x_7, x_8, x_9))
+    y = np.concatenate((y_0, y_1, y_2, y_3, y_4, y_5, y_6, y_7, y_8, y_9))
+    M = np.concatenate((M_0, M_1, M_2, M_3, M_4, M_5, M_6, M_7, M_8, M_9))
+
+    triang = tri.Triangulation(x, y)
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_aspect('equal')
+    tcf = ax1.tricontourf(triang, M)
+    fig1.colorbar(tcf)
 
 
     if lines:
         Lines(Val_0, Val_1, Val_2, Val_3, val_4, val_5, val_7, val_9, n, x_A, y_A)
 
-    ax = plt.gca()
-    ax.set_aspect('equal', adjustable='box')
     if plot:
         plt.show()
         
@@ -69,8 +81,3 @@ def Main():
     
 if __name__ == "__main__":
     Main()
-
-
-
-#FIX ISSUE WITH STRAIGHT LINE
-#FIX COLOR UNIFORMITY
